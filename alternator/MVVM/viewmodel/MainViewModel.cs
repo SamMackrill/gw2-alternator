bc6110ca165b48ccf0db9af8339b9ab8f6b05868
Sam Mackrill <sammackrill@gmail.com>
2021-12-08 12:45:08 +0000

    Add MainViewModel

---
+++ alternator/MVVM/viewmodel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using NLog;
+
+namespace guildwars2.tools.alternator
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand? LoginAllCommand { get; set; }
+
+        public MainViewModel()
+        {
+            LoginAllCommand = new RelayCommand(o => {
+                var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+                var accountsJson = "accounts.json";
+                var accounts = JsonSerializer.Deserialize<List<Account>>(File.ReadAllText(accountsJson));
+
+                var launcher = new ClientController(new FileInfo(Path.Combine(appData, @"Guild Wars 2\Local.dat")));
+                launcher.Launch(accounts.Where(a => a.LastSuccess < DateTime.UtcNow.Date));
+
+                File.WriteAllText(accountsJson, JsonSerializer.Serialize(accounts, new JsonSerializerOptions { AllowTrailingCommas = true, WriteIndented = true }));
+                LogManager.Shutdown();
+            });
+        }
+    }
+}
