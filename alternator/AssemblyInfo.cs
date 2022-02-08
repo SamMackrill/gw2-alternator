11a77ec5eadbb35d7a5a4512f7681ce4f4945224
Sam Mackrill <sammackrill@gmail.com>
2022-02-08 14:11:31 +0000

    Version is set in the project now

--- alternator/AssemblyInfo.cs
+++ alternator/AssemblyInfo.cs
@@ -1,5 +1,3 @@
-using System.Reflection;
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
@@ -8,5 +6,3 @@ using System.Reflection;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
