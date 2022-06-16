b8e4b4459ff310dcb5fdc6149686dfe03e65c81c
Sam Mackrill <sammackrill@gmail.com>
2022-06-16 08:58:01 +0100

    Fix

--- alternator/MVVM/model/ClientController.cs
+++ alternator/MVVM/model/ClientController.cs
@@ -150,7 +150,7 @@ public class ClientController
                 catch (OperationCanceledException ce)
                 {
                     authenticationThrottle.Reset();
-                    Logger.Debug("Cancelled {0} clients", clientsToLaunch.Count, ce.CancellationToken.);
+                    Logger.Debug("Cancelled {0} clients", clientsToLaunch.Count);
                     return;
                 }
                 catch (Exception e)
